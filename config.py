664ccc4b6affcdbe1221f9d4df37b91c7b3ba47d
KotVasily <163335693+KotVasily@users.noreply.github.com>
2024-08-16 10:53:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # Настройки Naga AI
 api_key = "Введите ключ Naga AI" # Его можно получить в discord канале NagaAI
-api_key_tg = "Введите ключ TG бота"#"6676487368:AAFxFgu_Nsw9HMm6bbWYpyeC0pR1Gw55S70"
+api_key_tg = "Введите ключ TG бота"
