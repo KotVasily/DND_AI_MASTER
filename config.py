0dfa9da4bd0d7ef2e84504c767f52a7a47cbddad
KotVasily <163335693+KotVasily@users.noreply.github.com>
2024-08-16 10:52:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
 # Настройки Naga AI
-api_key = "ng-Mce0MiuZDH07Tmh3vc0jn8HMOWhrG"
-api_key_tg = "6676487368:AAFxFgu_Nsw9HMm6bbWYpyeC0pR1Gw55S70"
-# Настройки TTS
-language = 'ru'
-model_id = 'ru_v3'
-sample_rate = 48000  # 48000
-speaker = 'xenia'  # aidar, baya, kseniya, xenia, random
-put_accent = True
-put_yo = True
+api_key = "Введите ключ Naga AI" # Его можно получить в discord канале NagaAI
+api_key_tg = "Введите ключ TG бота"#"6676487368:AAFxFgu_Nsw9HMm6bbWYpyeC0pR1Gw55S70"
